ddc92b1d3271fa75fbd6072e731d8b00bd31066a
belalfawzy <belalfawzy123@gmail.com>
2025-04-18 18:41:02 +0200

    Contollers

---
+++ Marketteto/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Marketteto.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Marketteto.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly MarkettetoDbContext _context;
+        public CategoryController(MarkettetoDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var res = await _context.Categories.ToListAsync();
+            return View();
+        }
+    }
+}
---
+++ Marketteto/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Marketteto.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Marketteto.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly MarkettetoDbContext _context;
+        public ProductController(MarkettetoDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var res = await _context.Products.ToListAsync();
+            return View();
+        }
+    }
+}
