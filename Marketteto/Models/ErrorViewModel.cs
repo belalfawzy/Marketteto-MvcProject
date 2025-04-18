7fa04d59fe4251869eacb09b3113ddb619e071f9
belalfawzy <belalfawzy123@gmail.com>
2025-04-18 16:50:07 +0200

    Add project files.

---
+++ Marketteto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Marketteto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
